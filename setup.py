fc7b91cd7be8e147756a8f1860be88c3f5c289e5
Jonathan Porta <jonathan@jonathanporta.com>
2017-11-19 00:03:25 -0700

    Fix a typo

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'safe_extractor': [], # TODO: Separate out the ui from the framework
+        'safe_extractor': ['../README.md', '../VERSION', '../requirements.txt'], # TODO: Separate out the ui from the framework
     },
 
     # Although 'package_data' is the preferred approach, in some case you may

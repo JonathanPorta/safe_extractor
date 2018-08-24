767625596d8076a18fd78ef3e3fb76bdf10e6d14
Jonathan Porta <jonathan@jonathanporta.com>
2018-08-24 00:42:13 -0600

    Fix issue with pip version > 10
    Fix from: https://stackoverflow.com/a/49867265/555017

--- setup.py
+++ setup.py
@@ -22,7 +22,10 @@ with open(path.join(here, 'VERSION'), encoding='utf-8') as f:
     version = f.read()
 
 # Include installed pip requirements
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements('requirements.txt', session=False)
